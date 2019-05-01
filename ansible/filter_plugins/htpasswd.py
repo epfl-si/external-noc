73ddbdfa92fe91331ad5e1d676e422d0c049429c
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2019-05-01 18:39:10 +0200

    Fix and clean
     - add alertmanager
     - add blackbox
     - add grafana
     - generalize c2c export rule
     - fix htpasswd generator plugin
     - get rid of global htpasswd file and have demo a per-host solution
     - get rid of .env file
     - move prometheus data to persistent local volume
     - group all ports into the same namespace for better readability

--- ansible/filter_plugins/htpasswd.py
+++ ansible/filter_plugins/htpasswd.py
@@ -1,23 +1,36 @@
-# usage
-## Transform clear text username:password into htpasswd encrypted hash.
-import bcrypt
+# Transform clear text list of username:password into corresponding entry
+# suitable for traefik traefik.frontend.auth.basic label
+# Keeps the salt in a tmp file so that runs are more or less idempotents.
+
+from bcrypt import gensalt, hashpw
 import os
 import re
 
-def htpasswd(arg, salt=None):
-  if salt is None:
-    esalt = bcrypt.gensalt()
+# Oops... just found that there is a standard hash filter in ansible
+#         well in the end it does not simply much   :p
+# from ansible.plugins.filter.core import get_encrypted_password
+# from ansible.utils.encrypt import random_salt
+
+def htpasswd(arg, resalt=False, path='/tmp/ansible_htpasswd_salt.txt'):
+  if (resalt or not os.path.exists(path)):
+    salt = gensalt()
+    f = open(path,"wb")
+    f.write(salt)
+    f.close()
   else:
-    esalt = bytes(salt, 'utf-8')
+    f = open(path, "rb")
+    salt = f.read()
+    f.close()
+
   ups=re.split(r'\s+', arg)
   ueps=[]
   for up in ups:
     u, p = up.split(":")
-    # ep = crypt(p)
-    ep = bcrypt.hashpw(bytes(p, 'utf-8'), esalt)
+    ep = hashpw(bytes(p, 'utf-8'), salt)
     ep = str(ep, 'utf-8')
     ueps.append("%s:%s" % (u, ep))
   output = ", ".join(ueps)
+  output = re.sub('\$', '$$', output)
   return output
 
 class FilterModule(object):
