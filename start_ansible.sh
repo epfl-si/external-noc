5026e9f401def9e6e8fa3e40f2be253d9e7953d5
Christian Zufferey <christian@zufferey.com>
2019-05-27 14:11:17 +0000

    wip

--- start_ansible.sh
+++ start_ansible.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 #Petit script pour démarrer tout le binz
-#zf190527.1531
+#zf190527.1604
 
 
 cd ansible
 ansible-playbook -i hosts.yml playbook.yml
 
+
+
 echo -e "
 
+il faut redémarrer le service prometheus avec:
 
+ssh root@prometheus.idev-fsd.ml \"cd /root/anoc2/noc ; docker-compose restart prometheus\"
+
+docker-compose restart prometheus
 
 ne PAS oublier quand tout est terminé de faire
 
 
 rm ./ansible/keys/private_key.pkcs7.pem
 
+
+
+Pour ajouter la machine à surveiller:
+
+nano ~/external-noc/ansible/idevfsd-noc/vars/main.yml
 
 "
 
