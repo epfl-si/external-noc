a6c317265f13244db7bf247adfccea2010dcc343
Nicolas Borboën <ponsfrilus@gmail.com>
2019-09-11 17:41:35 +0200

    [FIX] Python3's now happy (at least on my computer)

--- ansible/filter_plugins/htpasswd.py
+++ ansible/filter_plugins/htpasswd.py
@@ -14,21 +14,19 @@ import re
 def htpasswd(arg, resalt=False, path='/tmp/ansible_htpasswd_salt.txt'):
   if (resalt or not os.path.exists(path)):
     salt = gensalt()
-    f = open(path,"wb")
+    f = open(path,"w")
     f.write(salt)
     f.close()
   else:
-    f = open(path, "rb")
+    f = open(path, "r")
     salt = f.read()
     f.close()
-  
+
   ups=re.split(r'\s+', arg)
   ueps=[]
   for up in ups:
     u, p = up.split(":")
     ep = hashpw(bytes(p, 'utf-8'), salt)
-    # FIXME: Python 3 only
-    ep = str(ep, 'utf-8')
     ueps.append("%s:%s" % (u, ep))
   output = ", ".join(ueps)
   output = re.sub('\$', '$$', output)
