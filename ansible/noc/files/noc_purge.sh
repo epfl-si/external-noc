222221a4b92f146a7bd89e42cef1f6685d08c47e
Christian Zufferey <christian@zufferey.com>
2020-08-10 12:16:24 +0200

    Clearing zf_commands.md and translating noc_purge.sh

--- ansible/noc/files/noc_purge.sh
+++ ansible/noc/files/noc_purge.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-#Petit script pour nettoyer tout le binz du NOC
-#zf200429.1522
+#Small script to clean all the NOC binz
+#zf200810.1215
 
 #source: http://patatos.over-blog.com/2016/09/commet-faire-du-menage-dans-les-conteneurs-et-images-docker.html
 
 echo -e "
-Arrête le service et enlève le container ET aussi les datas
-"
+Stop the service and remove the container AND also the data"
 
-read -p "Etes-vous certain de vouloir tout effacer ?"
-read -p "Mais cela va VRAIMENT VRAIMENT tout effacer, y compris les data !"
+read -p "Are you sure you want to erase everything ?"
+read -p "But this will REALLY REALLY erase everything, including the data !"
 
-echo -e "\n\nArrête tous les containers et les efface !\n"
+echo -e "\n\nStop all the containers and erase them !\n"
 docker rm -f -v traefik blackbox-exporter noc_nodeexporter_1 noc_prometheus_1 noc_pushgateway_1 noc_grafana_1
 
-echo -e "\n\nEfface les images !\n"
+echo -e "\n\nErase the images !\n"
 docker image rm traefik:1.7 grafana/grafana prom/prometheus prom/pushgateway quay.io/prometheus/node-exporter prom/blackbox-exporter:v0.14.0
 
-echo -e "\n\nEfface les networks !\n"
+echo -e "\n\nErase the networks !\n"
 docker network prune -f
 
-echo -e "\n\nAffiche le résultat !\n"
+echo -e "\n\nShow the result !\n"
 docker container ls -a
 echo -e "\n"
 docker image ls
