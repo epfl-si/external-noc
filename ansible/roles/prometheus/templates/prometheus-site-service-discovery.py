32487d903cb9d6e0b6ccaf7f6baad73d4aae58ea
Nicolas Borboën <ponsfrilus@gmail.com>
2020-10-06 10:42:15 +0200

    [feature] configurator uses file rename
    add starting log with a date

--- ansible/roles/prometheus/templates/prometheus-site-service-discovery.py
+++ ansible/roles/prometheus/templates/prometheus-site-service-discovery.py
@@ -6,6 +6,7 @@ of dynamic targets for it.
 See https://github.com/epfl-si/wp-ops/blob/master/ansible/roles/wordpress-openshift-namespace/templates/prometheus-menu-service-discovery.py
 """
 
+from datetime import datetime
 import json
 import logging
 import os
@@ -13,10 +14,22 @@ import time
 import traceback
 import urllib.request
 
-
 class DynamicConfig:
-    def __init__(self, url="https://wp-veritas.epfl.ch/api/v1/sites"):
+
+    targetPath = "/srv/dynamic/targets.json"
+
+    def __init__(self, url="https://wp-veritas.epfl.ch/api/v1/sites", targetPath=None, frequency=300):
         self.url = url
+        if targetPath:
+            self.targetPath = targetPath
+        else:
+            self.targetPath = DynamicConfig.targetPath
+        self.frequency = frequency
+        print(f"{datetime.now().isoformat()} Configurator started !"
+              f"\n\t - url: {self.url}"
+              f"\n\t - dynamic file: {self.targetPath}"
+              f"\n\t - frequency: {self.frequency}s",
+            flush=True)
 
     def _get_json(self):
         res = urllib.request.urlopen(self.url)
@@ -41,14 +54,23 @@ class DynamicConfig:
             targets_by_wp_env.setdefault(wp_env, []).append(url)
         return targets_by_wp_env.items()
 
-while True:
-    try:
-        with open("/srv/dynamic/targets.json", "w") as f:
-            f.write(json.dumps([
+    def _write(self, struct):
+        tmpTarget = self.targetPath + '.tmp'
+        with open(tmpTarget, "w") as f:
+            f.write(json.dumps(struct))
+        os.rename(tmpTarget, self.targetPath)
+
+    def write_targets(self):
+        self._write([
                 dict(
                     targets=targets,
                     labels=dict(wp_env=wp_env))
-                for wp_env, targets in DynamicConfig().enumerate()]))
+                for wp_env, targets in self.enumerate()])
+
+dc = DynamicConfig()
+while True:
+    try:
+        dc.write_targets()
     except:  # noqa
         logging.error(traceback.format_exc())
-    time.sleep(300)
+    time.sleep(dc.frequency)
